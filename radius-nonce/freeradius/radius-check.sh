c16ed8e40ecc21b3f8e35f4a0554d97efd1edd0a
Rostislav Kondratenko <r.kondratenko@wwpass.com>
2020-10-19 17:09:54 +0300

    Add NAS-Identifier as a substitute for ASA-TunnelGroupName for OpenVPN

--- radius-nonce/freeradius/radius-check.sh
+++ radius-nonce/freeradius/radius-check.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
-if [[ $# < 4 ]] ; then
+if [[ $# < 5 ]] ; then
         echo 'You must provide user name, Tunnel group name, RADIUS nonce'
         exit 1
 fi
 
 username=$1
 profile=$2
-nonce=$3
-port=$4
+nasid=$3
+nonce=$4
+port=$5
+
+if [[ x$profile == x ]]; then
+	profile=$nasid
+fi
 RETVAL=
 
 curl --silent -X POST --data-urlencode username=$username --data-urlencode profile=$profile --data-urlencode nonce=$nonce --fail http://localhost:$port/api/v1/check
